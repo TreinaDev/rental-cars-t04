c38425d131a8968a9f0012a1fcc22f8830689344
Henrique Morato <riquemorato@gmail.com>
2020-08-21 16:53:21 -0300

    Agendamento de locação com sucesso

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :car_categories
   resources :subsidiaries, only: [:index, :show, :new, :create]
   resources :car_models, only: [:index, :show, :new, :create]
+  resources :rentals, only: [:index, :show, :new, :create]
 end
