79f1d214c3a5257e940081a7ee504f65e911366f
Henrique Morato <riquemorato@gmail.com>
2020-09-22 17:35:33 -0300

    API de Carro e autorização de categoria

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,6 +2,8 @@ class Car < ApplicationRecord
   belongs_to :car_model
   has_many :car_rentals
 
+  validates :license_plate, :color, presence: true
+
   enum status: { available: 0, rented: 10 }
 
   def description
